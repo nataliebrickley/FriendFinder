461c4a381549a30e1ea546f0f1ce6683579c06e9
Natalie Brickley <natt_rose@yahoo.com>
2019-03-29 13:33:26 -0700

    added best match algorithm

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,8 +8,22 @@ module.exports = (app) => {
     })
     app.post("/api/friends", (req, res) => {
         //handle incoming survey results
+        let newFriend = req.body
+        let bestMatch = [40, "name", "photo"]
+        friends.forEach(person => {
+            let totalDiff = 0 
+            for(let i=0; i<person.scores.length; i++) {
+                totalDiff += Math.abs(parseInt(person.scores[i]) - parseInt(newFriend.scores[i]))
+            }
+            if (totalDiff <= bestMatch[0]) {
+                bestMatch[0] = totalDiff;
+                bestMatch[1] = person.name;
+                bestMatch[2] = person.photo;
+            }   
+        })
+
         //console.log(req.body)
         friends.push(req.body);
-        res.json(true);
+        res.json(bestMatch);
     })
 }
