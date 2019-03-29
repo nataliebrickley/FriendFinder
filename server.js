b5e1fe463c4f141ac44122f8b9b0dfaab9456790
Natalie Brickley <natt_rose@yahoo.com>
2019-03-28 17:40:37 -0700

    made a /survey route

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 const express = require('express');
+const app = express();
 const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+//require("/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+  });
+  
