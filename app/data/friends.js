e8aa29cd80e2f84aa2f015e1911f49d96aaf99bd
Natalie Brickley <natt_rose@yahoo.com>
2019-03-30 11:54:27 -0700

    updated css and friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,53 @@
 let friends = [
     {
-        "name": "Natalie",
-        "photo": "https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-        "scores": [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+        "name": "Rose",
+        "photo": "https://cdn.crownmediadev.com/c6/8a/b8a58c2f4a5caa4304947eb88b76/thegoldengirls-0017-bw.jpg",
+        "scores": [5, 2, 1, 2, 5, 2, 4, 1, 5, 4]
+    },
+    {
+        "name": "Blanche",
+        "photo": "https://peopledotcom.files.wordpress.com/2016/08/rue-mcclanahan.jpg",
+        "scores": [4, 3, 2, 2, 3, 3, 3, 2, 3, 5]
+    },
+    {
+        "name": "Dorothy",
+        "photo": "http://www.90s411.com/images/golden-girls-costume-dorothy-zbornak.jpg",
+        "scores": [3, 5, 4, 4, 2, 4, 4, 5, 2, 3]
+    },
+    {
+        "name": "Sophia",
+        "photo": "https://akns-images.eonline.com/eol_images/Entire_Site/201476/rs_1024x759-140806172825-1024.golden-girls-sophia.jpg",
+        "scores": [4, 5, 5, 3, 4, 2, 5, 4, 2, 3]
+    },
+    {
+        "name": "Rachel",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2014/12/17/17-rachel-green-jewish.w700.h700.jpg",
+        "scores": [2, 3, 4, 3, 1, 3, 2, 3, 2, 5]
+    },
+    {
+        "name": "Phoebe",
+        "photo": "https://media1.popsugar-assets.com/files/thumbor/L6u2EA_RHtV_piy8CDItMVqiUQ4/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2014/11/04/740/n/1922283/b873d68328a0eef2_Phoebe-Top.jpg",
+        "scores": [1, 2, 3, 2, 5, 4, 1, 1, 4, 4]
+    },
+    {
+        "name": "Ross",
+        "photo": "https://pixel.nymag.com/imgs/fashion/daily/2018/10/24/24-ross-friends.w700.h700.jpg",
+        "scores": [2, 3, 3, 4, 2, 2, 4, 2, 3, 3]
+    },
+    {
+        "name": "Chandler",
+        "photo": "https://pixel.nymag.com/imgs/daily/vulture/2017/03/30/30-chandler-bing.w330.h330.jpg",
+        "scores": [3, 4, 2, 3, 3, 4, 3, 4, 2, 2]
+    },
+    {
+        "name": "Monica",
+        "photo": "http://www2.pictures.zimbio.com/mp/Cw-NvzmXZ9Wl.jpg",
+        "scores": [1, 4, 2, 4, 3, 5, 4, 2, 2, 3]
+    },
+    {
+        "name": "Joey",
+        "photo": "http://az616578.vo.msecnd.net/files/2016/01/24/635891938267101138434991188_sddefault.jpg",
+        "scores": [5, 1, 4, 2, 4, 5, 2, 1, 5, 4]
     }
 ]
 
